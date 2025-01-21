914a9162b3570957d847c95324efbef62c4e865f
Cli7max <juritishivani@gmail.com>
2025-01-21 20:22:24 +0530

    Add files via upload

---
+++ Address,bug.go
@@ -0,0 +1,21 @@
+//Expected query for address: cosmos1exampleaccount
+//Actual query made for address: cosmos1hardcodedaddress
+
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+// Mock implementation for demonstration
+func TestHardcodedAddressBug(addr string, hardcodedAddress string) {
+	if addr != hardcodedAddress {
+		log.Printf("Bug Detected: Expected query for address %s, but queried hardcoded address %s", addr, hardcodedAddress)
+	}
+}
+
+func main() {
+	TestHardcodedAddressBug("cosmos1exampleaccount", "cosmos1hardcodedaddress")
+}
+
